0c30833c3dd0a25a35c71c4a656773f560145b87
NTK1712 <106008858+NTK1712@users.noreply.github.com>
2022-05-25 21:17:36 +0700

    change

--- OOP/Header.h
+++ OOP/Header.h
@@ -4,34 +4,46 @@
 #include <string>
 #include <cstdlib>
 #include <time.h>
+#include <vector>
+#include <Windows.h>
 using namespace std;
-
-void BubbleSort(int A[], int n);
-
+//1
 void SelectionSort(int A[], int n);
+//2
 
+//3
 void InsertionSort(int A[], int n);
+//4
 
+//5
+void BubbleSort(int A[], int n);
+//6
+
+//7
+
+//8
+void CountingSort(int A[], int n);
+//9
+int getMax(int A[], int n);//lấy giá trị lớn nhất trong mảng
+void RadixSort(int A[], int n);
+//10
 void heapify(int A[], int n, int i);
 void HeapSort(int A[], int n);
-
+//11
+int partition(int A[], int low, int high);
+void QuickSort(int A[], int n);
+//12
 void Merge(int A[], int left, int mid, int right);
 void MergeS(int A[], int left, int right);
 void MergeSort(int A[], int n);
 
-int partition(int A[], int low, int high);
-void QuickSort(int A[], int n);
 
-void CountingSort(int A[], int n);
-void CountingSort(int A[], int n, int div);
 
-void RadixSort(int arr[], int n);
 
-int getMax(int A[], int n);//lấy giá trị lớn nhất trong mảng
-
-void setArrayAuto(int A[], int n);//tự set mảng random
-
-void input1(int A[], int n);
-void input2(int A[], int n);
-void input3(int A[], int n);
+void input1(int A[], int n);//nhập trực tiếp từng số
+void input2(int A[], int n);//nhập ngẫu nhiên 
+void input3(int A[], int n);//nhập qua file
 void input(int A[]);
+
+void chose(int A[]);
+void Menu();
