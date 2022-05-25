bf34f8ab0bab522640cf767b729c0dcd9801b98a
hgbao226 <21520604@gm.uit.edu.vn>
2022-05-25 20:39:59 +0700

    change source.cpp

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,20 +1,10 @@
 #include "Header.h"
 
-/*int main()
-{
-    srand(unsigned(time(NULL)));
-    int A[50000];
-    setArrayAuto(A, 50000);
-    CountingSort(A, 50000);
-    system("pause");
-    return 0;
-}*/
 int main()
 {
     srand(unsigned(time(NULL)));
     int A[50000];
-    setArrayAuto(A, 5);
-    RadixSort(A, 5);
+    setArrayAuto(A, 50000);
     system("pause");
     return 0;
 }
--- OOP/Source.cpp
+++ OOP/Source.cpp
@@ -1,10 +1,5 @@
 ﻿#include "Header.h"
-/*khởi tạo giá trị cho mảng*/
-void setArrayAuto(int A[], int n) {
-	for (int i = 0; i < n; i++) {
-		A[i] = rand() % 100000 + 0;
-	}
-}
+//trả về giá trị max trong mảng
 int getMax(int A[], int n) {
 	int max = A[0];
 	for (int i = 0; i < n; i++)
