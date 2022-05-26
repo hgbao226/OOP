e3932c245ccc1c76ab0b465804b58c473a8aae7d
NTK1712 <106008858+NTK1712@users.noreply.github.com>
2022-05-26 13:19:27 +0700

    Update heap_sort.cpp

--- OOP/heap_sort.cpp
+++ OOP/heap_sort.cpp
@@ -4,29 +4,80 @@ void heapify(int A[], int n, int i)
 {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
-    int largest = i;
+    int Max = i;
 
-    if (left < n && A[left] < A[largest])
-        largest = left;
-    if (right < n && A[left] < A[largest])
-        largest = right;
+    if (left < n && A[left] < A[Max])
+        Max = left;
+    if (right < n && A[left] < A[Max])
+        Max = right;
 
-    if (largest != i)
+    if (Max != i)
     {
-        swap(A[i], A[largest]);
-        heapify(A, n, largest);
+        swap(A[i], A[Max]);
+        heapify(A, n, Max);
     }
 }
-
 void HeapSort(int A[], int n)
 {
     for (int i = n / 2 - 1; i >= 0; i--)
     {
         heapify(A, n, i);
     }
-    for (int i = n - 1; i >= 0; i--)
+    for (int j = n - 1; j > 0; j--)
     {
-        swap(A[i], A[0]);
-        heapify(A, i, 0);
+        swap(A[j], A[0]);
+        heapify(A, j, 0);
     }
+}
+
+void heapifyDE(int A[], int n, int i)
+{
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    int Min = i;
+
+    if (left < n && A[left] > A[Min])
+        Min = left;
+    if (right < n && A[left] > A[Min])
+        Min = right;
+
+    if (Min != i)
+    {
+        swap(A[i], A[Min]);
+        heapify(A, n, Min);
+    }
+}
+void HeapSortDE(int A[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapifyDE(A, n, i);
+    }
+    for (int j = n - 1; j > 0; j--)
+    {
+        swap(A[j], A[0]);
+        heapifyDE(A, j, 0);
+    }
+}
+
+
+void Heap_Sort(int A[], int n)
+{
+    int ch;
+    do
+    {
+        cout << "\n1.Sap xep tang dan\n2.Sap xep giam dan\n3.Quay lai!\n";
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+            HeapSort(A, n);
+            break;
+        case 2:
+            HeapSortDE(A, n);
+            break;
+        default:
+            break;
+        }
+    } while (ch != 3);
 }
