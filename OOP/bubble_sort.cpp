ed2844cfe6d7f2f16b8332a3dbb853f86969ac5f
NTK1712 <106008858+NTK1712@users.noreply.github.com>
2022-05-26 13:20:06 +0700

    Update bubble_sort.cpp

--- OOP/bubble_sort.cpp
+++ OOP/bubble_sort.cpp
@@ -7,3 +7,30 @@ void BubbleSort(int A[], int n)
             if (A[j] > A[j + 1])
                 swap(A[j], A[j + 1]);
 }
+void BubbleSortDE(int A[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (A[j] < A[j + 1])
+                swap(A[j], A[j + 1]);
+}
+void Bubble_Sort(int A[], int n)
+{
+    int ch;
+    do
+    {
+        cout << "\n1.Sap xep tang dan\n2.Sap xep giam dan\n3.Quay lai!\n";
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+            BubbleSort(A, n);
+            break;
+        case 2:
+            BubbleSortDE(A, n);
+            break;
+        default:
+            break;
+        }
+    } while (ch != 3);
+}
