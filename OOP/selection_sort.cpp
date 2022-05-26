9595808ba1e8b23389183c7ca404b3ebc5d49352
NTK1712 <106008858+NTK1712@users.noreply.github.com>
2022-05-26 13:18:33 +0700

    Update selection_sort.cpp

--- OOP/selection_sort.cpp
+++ OOP/selection_sort.cpp
@@ -7,9 +7,42 @@ void SelectionSort(int A[], int n)
     {
         min = i;
         for (int j = i + 1; j < n; j++)
-            if (A[j] < A[min])
-                min = j;
-        if (min != i)
-            swap(A[i], A[min]);
+        {
+            if (A[j] < A[min])  min = j;
+        }
+        swap(A[i], A[min]);
     }
+}
+void SelectionSortDE(int A[], int n)
+{
+    int max;
+    for (int i = 0; i < n - 1; i++)
+    {
+        max = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (A[j] > A[max])  max = j;
+        }
+        swap(A[i], A[max]);
+    }
+}
+void Selection_Sort(int A[], int n)
+{
+    int ch;
+    do
+    {
+        cout << "\n1.Sap xep tang dan\n2.Sap xep giam dan\n3.Quay lai!\n";
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+            SelectionSort(A, n);
+            break;
+        case 2:
+            SelectionSortDE(A, n);
+            break;
+        default:
+            break;
+        }
+    } while (ch != 3);
 }
